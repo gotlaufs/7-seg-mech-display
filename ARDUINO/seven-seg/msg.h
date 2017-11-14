a46e26a7ba29ff3e534faf9496b48ab2efc4188f
Roberts Gotlaufs <gotlaufs@gmail.com>
2017-11-14 23:37:51 +0200

    Add new Arduino algorithm with basic text console

---
+++ ARDUINO/seven-seg/msg.h
@@ -0,0 +1,122 @@
+/*
+ * Static strings
+ *
+ * Refer to:
+ * https://playground.arduino.cc/Main/PROGMEM
+ */
+
+#ifndef _MSG_H
+#define _MSH_H
+#include <avr/pgmspace.h>
+
+// Help message
+const PROGMEM char MSG_HELP_0[] = "The seven segment display will ";
+const PROGMEM char MSG_HELP_1[] = "display messages that are sent ";
+const PROGMEM char MSG_HELP_2[] = "to it using the SAY command.\n";
+const PROGMEM char MSG_HELP_3[] = " >> SAY <message> - display the";
+const PROGMEM char MSG_HELP_4[] = " <message> letter by letter on ";
+const PROGMEM char MSG_HELP_5[] = "the 7-segment display to the ";
+const PROGMEM char MSG_HELP_6[] = "best of it's abilities.\n\n";
+const PROGMEM char MSG_HELP_7[] = "There ar few commands available";
+const PROGMEM char MSG_HELP_8[] = " to configure the message ";
+const PROGMEM char MSG_HELP_9[] = "display behaviour:\n";
+const PROGMEM char MSG_HELP_10[] = " >> LETTER_DELAY <ms> - how ";
+const PROGMEM char MSG_HELP_11[] = "much time to wait (in ";
+const PROGMEM char MSG_HELP_12[] = "milliseconds) between ";
+const PROGMEM char MSG_HELP_13[] = "displaying each letter. ";
+const PROGMEM char MSG_HELP_14[] = "Minimum 200.\n";
+const PROGMEM char MSG_HELP_15[] = " >> WORD_DELAY <ms> - how much ";
+const PROGMEM char MSG_HELP_16[] = "time to wait (in milliseconds) ";
+const PROGMEM char MSG_HELP_17[] = "between individual words in a ";
+const PROGMEM char MSG_HELP_18[] = "sentence. Minimum 200.\n";
+const PROGMEM char MSG_HELP_19[] = " >> BLANK <ON;OFF> - enable or ";
+const PROGMEM char MSG_HELP_20[] = "disable the blanking ";
+const PROGMEM char MSG_HELP_21[] = "(whitespace) between displaying";
+const PROGMEM char MSG_HELP_22[] = " each letter on the 7-seg ";
+const PROGMEM char MSG_HELP_23[] = "indicator.\n\n";
+const PROGMEM char MSG_HELP_24[] = "There are also few other ";
+const PROGMEM char MSG_HELP_25[] = "commands:\n";
+const PROGMEM char MSG_HELP_26[] = " >> HELP or just H - print this";
+const PROGMEM char MSG_HELP_27[] = " message.\n";
+const PROGMEM char MSG_HELP_28[] = " >> CONFIG - print out current ";
+const PROGMEM char MSG_HELP_29[] = "configuration.\n";
+const PROGMEM char MSG_HELP_31[] = " >> ABOUT - print out the intro";
+const PROGMEM char MSG_HELP_32[] = " message.\n";
+const PROGMEM char MSG_HELP_33[] = "Have fun!\n\n";
+
+const char * const MSG_HELP_TABLE[] PROGMEM= {
+	MSG_HELP_0,
+	MSG_HELP_1,
+	MSG_HELP_2,
+	MSG_HELP_3,
+	MSG_HELP_4,
+	MSG_HELP_5,
+	MSG_HELP_6,
+	MSG_HELP_7,
+	MSG_HELP_8,
+	MSG_HELP_9,
+	MSG_HELP_10,
+	MSG_HELP_11,
+	MSG_HELP_12,
+	MSG_HELP_13,
+	MSG_HELP_14,
+	MSG_HELP_15,
+	MSG_HELP_16,
+	MSG_HELP_17,
+	MSG_HELP_18,
+	MSG_HELP_19,
+	MSG_HELP_20,
+	MSG_HELP_21,
+	MSG_HELP_22,
+	MSG_HELP_23,
+	MSG_HELP_24,
+	MSG_HELP_25,
+	MSG_HELP_26,
+	MSG_HELP_27,
+	MSG_HELP_28,
+	MSG_HELP_29,
+	MSG_HELP_31,
+	MSG_HELP_32,
+	MSG_HELP_33
+};
+
+const int MSG_HELP_LEN = 33;
+
+// About message
+const PROGMEM char MSG_ABOUT_0[] = "_______________________________";
+const PROGMEM char MSG_ABOUT_1[] = "_______________________________";
+const PROGMEM char MSG_ABOUT_2[] = "    ____                       ";
+const PROGMEM char MSG_ABOUT_3[] = "   /    /                      ";
+const PROGMEM char MSG_ABOUT_4[] = "-------/--------__----__----__-";
+const PROGMEM char MSG_ABOUT_5[] = "      /   ===  (_ ` /___) /   )";
+const PROGMEM char MSG_ABOUT_6[] = "_____/________(__)_(___ _(___/_";
+const PROGMEM char MSG_ABOUT_7[] = "                            /  ";
+const PROGMEM char MSG_ABOUT_8[] = "                        (_ /   ";
+const PROGMEM char MSG_ABOUT_9[] = "-------------------------------";
+const PROGMEM char MSG_ABOUT_10[] = " A parrot seven segment display";
+const PROGMEM char MSG_ABOUT_11[] = "";
+const PROGMEM char MSG_ABOUT_12[] = "Type SAY <message> for action!";
+const PROGMEM char MSG_ABOUT_13[] = "Or type HELP for more info.";
+const PROGMEM char MSG_ABOUT_14[] = "\n";
+
+const char * const MSG_ABOUT_TABLE[] PROGMEM= {
+	MSG_ABOUT_0,
+	MSG_ABOUT_1,
+	MSG_ABOUT_2,
+	MSG_ABOUT_3,
+	MSG_ABOUT_4,
+	MSG_ABOUT_5,
+	MSG_ABOUT_6,
+	MSG_ABOUT_7,
+	MSG_ABOUT_8,
+	MSG_ABOUT_9,
+	MSG_ABOUT_10,
+	MSG_ABOUT_11,
+	MSG_ABOUT_12,
+	MSG_ABOUT_13,
+	MSG_ABOUT_14
+};
+
+const int MSG_ABOUT_LEN = 14;
+
+#endif // _MSG_H
---
+++ ARDUINO/seven-seg/seven-seg.ino
@@ -0,0 +1,257 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <avr/pgmspace.h>
+
+#include "alphabet.h"
+#include "msg.h"
+
+#define SEG_DRIVE_TIME 200 // How long apply current to each segment (ms)
+
+// Order is important. Segments:
+// G F E D C B A
+const char SEG_UP[7] = {7, 3, 6, 8, 5, 4, 2};
+const char SEG_DOWN[7] = {A0, 10, 12, A1, 13, 11, 9};
+
+boolean ECHO = false;	// Echo back Serial characters is using standard terminal
+boolean BLANK = true;	// Blank between letters
+int LETTER_DELAY = 500;	// Delay between letters in milliseconds
+int WORD_DELAY = 1000;	// Dealy between words in the text to say
+
+// '1' in bit means segmet is UP
+// Organized in 0x0gfe'dcba
+uint8_t CURRENT_SEG_STATE = 0x00;
+
+char string_buffer[32];
+int i;
+
+String in_string = "";
+
+// Helpers
+void sayString(String text);
+void printHelp(void);
+void printAbout(void);
+void printConfig(void);
+
+void setup(void){
+	Serial.begin(9600);
+	printAbout();
+
+	// Pin setup:
+	for (i=0; i<7; i++){
+		pinMode(SEG_UP[i], OUTPUT);
+		pinMode(SEG_DOWN[i], OUTPUT);
+	}
+}
+
+void loop(void){
+	char in_byte = 0;
+	int space_index = 0;
+	String command = "";
+	int working_int = 0;
+	if (Serial.available() > 0){
+		in_byte = Serial.read();
+
+		// Sanitary checks, because we support only standard ASCII table
+		if (!((in_byte > 127) || (in_byte < 0))){
+			in_string += in_byte;
+		}
+		else{
+			in_byte = 0;
+		}
+
+		if (ECHO == true){
+			Serial.print(in_byte);
+		}
+
+		if (in_byte == '\n'){
+			// Time to decode the in_string
+			// Remove last character (newline)
+			in_string = in_string.substring(0, in_string.length()-1);
+
+			space_index = in_string.indexOf(' ');
+			if (space_index > 0){
+				// Split between command and the rest
+				command = in_string.substring(0, space_index);
+				in_string = in_string.substring(space_index + 1);
+			}
+			else{
+				command = in_string;
+				in_string = "";
+			}
+
+			// Decode command
+			if (command.equalsIgnoreCase("SAY")){
+				sayString(in_string);
+			}
+			else if (command.equalsIgnoreCase("HELP")){
+				printHelp();
+			}
+			else if (command.equalsIgnoreCase("H")){
+				printHelp();
+			}
+			else if(command.equalsIgnoreCase("WORD_DELAY")){
+				working_int = in_string.toInt();
+				if (working_int < 200){
+					Serial.print("ERROR: Incorrect length for WORD_DELAY: <");
+					Serial.print(in_string);
+					Serial.println(">.");
+					Serial.println("Supported values are integers >200");
+				}
+				else{
+					WORD_DELAY = working_int;
+					Serial.println("OK");
+				}
+			}
+			else if(command.equalsIgnoreCase("LETTER_DELAY")){
+				working_int = in_string.toInt();
+				if (working_int < 200){
+					Serial.print("ERROR: Incorrect length for LETTER_DELAY: <");
+					Serial.print(in_string);
+					Serial.println(">.");
+					Serial.println("Supported values are integers >200");
+				}
+				else{
+					LETTER_DELAY = working_int;
+					Serial.println("OK");
+				}
+			}
+			else if(command.equalsIgnoreCase("BLANK")){
+				in_string.toUpperCase();
+				if(in_string.startsWith("ON")){
+					BLANK = true;
+					Serial.println("OK");
+				}
+				else if(in_string.startsWith("OFF")){
+					BLANK = false;
+					Serial.println("OK");
+				}
+				else{
+					Serial.print("ERROR: Incorrect parameter for BLANK: <");
+					Serial.print(in_string);
+					Serial.println(">.");
+					Serial.println("Supported values are: ON, OFF");
+				}
+			}
+			else if(command.equalsIgnoreCase("ECHO")){
+				in_string.toUpperCase();
+				if(in_string.startsWith("ON")){
+					ECHO = true;
+					Serial.println("OK");
+				}
+				else if(in_string.startsWith("OFF")){
+					ECHO = false;
+					Serial.println("OK");
+				}
+				else{
+					Serial.print("ERROR: Incorrect parameter for ECHO: <");
+					Serial.print(in_string);
+					Serial.println(">.");
+					Serial.println("Supported values are: ON, OFF");
+				}
+			}
+			else if(command.equalsIgnoreCase("CONFIG")){
+				printConfig();
+			}
+			else if(command.equalsIgnoreCase("ABOUT")){
+				printAbout();
+			}
+			else{
+				Serial.print("Unknown command: <");
+				Serial.print(command);
+				Serial.println(">");
+				printHelp();
+			}
+
+			in_string = "";
+		}
+	}
+}
+
+void printHelp(void){
+	for (i=0; i<MSG_HELP_LEN; i++){
+		strcpy_P(string_buffer, (char*)pgm_read_word(&(MSG_HELP_TABLE[i])));
+		Serial.print(string_buffer);
+	}
+}
+
+void printAbout(void){
+	for (i=0; i<MSG_ABOUT_LEN; i++){
+		strcpy_P(string_buffer, (char*)pgm_read_word(&(MSG_ABOUT_TABLE[i])));
+		Serial.println(string_buffer);
+	}
+}
+
+void printConfig(void){
+	Serial.println("Current configuration values:");
+	Serial.print(" >> LETTER_DELAY = ");
+	Serial.print(LETTER_DELAY);
+	Serial.println(" ms");
+	Serial.print(" >> WORD_DELAY = ");
+	Serial.print(LETTER_DELAY);
+	Serial.println(" ms");
+	Serial.print(" >> ECHO = ");
+	Serial.println(ECHO);
+	Serial.print(" >> BLANK = ");
+	Serial.println(ECHO);
+	Serial.println();
+}
+
+void sayString(String text){
+	Serial.print("Displaying data - observe..  ");
+	// CURRENT_SEG_STATE
+	int j;
+	// This will drive only those segments that need to be driven.
+	uint8_t working_char, current_bit, next_bit;
+	uint8_t drive_up[7], drive_down[7];
+	for (i=0; i<text.length(); i++){
+		if(text[i] == ' '){
+			// Word ended, just delay
+			delay(WORD_DELAY);
+			continue;
+		}
+
+		//working_char = pgm_read_word_near(ascii_lookup[text[i]]);
+		working_char = ascii_lookup[text[i]];
+
+		// Calculate drive signals
+		for (j=0; j<7; j++){
+			current_bit = CURRENT_SEG_STATE & (0x01<<j);
+			next_bit = working_char & (0x01<<j);
+
+			if(current_bit ^ next_bit){
+				// Current and segment to-display differ
+				// Need to drive it
+				if (next_bit){
+					// Positive means need to go UP
+					drive_up[j] = 1;
+					drive_down[j] = 0;
+				}
+				else{
+					drive_up[j] = 0;
+					drive_down[j] = 1;
+				}
+			}
+			else{
+				drive_up[j] = 0;
+				drive_down[j] = 0;
+			}
+		}
+
+		// Do the actual driving
+		for (j=0; j<7; j++){
+			digitalWrite(SEG_UP[j], drive_up[j]);
+			digitalWrite(SEG_DOWN[j], drive_down[j]);
+		}
+		delay(SEG_DRIVE_TIME);
+		for (j=0; j<7; j++){
+			digitalWrite(SEG_UP[j], 0);
+			digitalWrite(SEG_DOWN[j], 0);
+		}	
+
+		CURRENT_SEG_STATE = working_char;
+		delay(LETTER_DELAY);
+	}
+
+	Serial.println("DONE!");
+}
